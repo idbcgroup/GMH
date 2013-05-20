92d627d2f1b18b31742c90f3112173c4a9567866
angel lacret <alacret@hp-4geeks.(none)>
2013-05-20 10:12:24 -0430

    smartgwt test

--- webclient/src/main/java/org/fourgeeks/gha/webclient/client/Gha.java
+++ webclient/src/main/java/org/fourgeeks/gha/webclient/client/Gha.java
@@ -1,14 +1,8 @@
 package org.fourgeeks.gha.webclient.client;
 
-import org.fourgeeks.gha.domain.mix.LegalEntity;
-
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Style.VerticalAlign;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.types.VerticalAlignment;
+import com.smartgwt.client.widgets.Window;
 import com.smartgwt.client.widgets.form.DynamicForm;
 import com.smartgwt.client.widgets.form.fields.CheckboxItem;
 import com.smartgwt.client.widgets.form.fields.LinkItem;
@@ -25,53 +19,52 @@ public class Gha implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-	/*	
-		final GWTUserServiceAsync service = GWT.create(GWTUserService.class);
-		service.test(new AsyncCallback<Boolean>() {
-			@Override
-			public void onSuccess(Boolean result) {
-				Window.alert("" + result);
-				service.test2(new AsyncCallback<LegalEntity>() {
-
-					@Override
-					public void onFailure(Throwable caught) {
-						// TODO Auto-generated method stub
-
-					}
-
-					@Override
-					public void onSuccess(LegalEntity result) {
-						if (result != null)
-							Window.alert("success");
-						else
-							Window.alert("error");
-
-					}
-				});
-			}
-
-			
-			
-			@Override
-			public void onFailure(Throwable caught) {
-				// TODO Auto-generated method stub
-
-			}
-		});
-*/
+		/*
+		 * final GWTUserServiceAsync service = GWT.create(GWTUserService.class);
+		 * service.test(new AsyncCallback<Boolean>() {
+		 * 
+		 * @Override public void onSuccess(Boolean result) { Window.alert("" +
+		 * result); service.test2(new AsyncCallback<LegalEntity>() {
+		 * 
+		 * @Override public void onFailure(Throwable caught) { // TODO
+		 * Auto-generated method stub
+		 * 
+		 * }
+		 * 
+		 * @Override public void onSuccess(LegalEntity result) { if (result !=
+		 * null) Window.alert("success"); else Window.alert("error");
+		 * 
+		 * } }); }
+		 * 
+		 * 
+		 * 
+		 * @Override public void onFailure(Throwable caught) { // TODO
+		 * Auto-generated method stub
+		 * 
+		 * } });
+		 */
 		final DynamicForm loginForm = new DynamicForm();
 		loginForm.setWidth("400");
 		loginForm.setLayoutAlign(VerticalAlignment.CENTER);
 		final TextItem username = new TextItem("username", "Nombre De Usuario");
 		final PasswordItem password = new PasswordItem("password", "Contraseña");
-		final SubmitItem submitButton = new	SubmitItem("submitButton","Iniciar Sesion");
-		final CheckboxItem rememberData = new CheckboxItem("rememberData", "Recordar mis Datos");
+		final SubmitItem submitButton = new SubmitItem("submitButton",
+				"Iniciar Sesion");
+		final CheckboxItem rememberData = new CheckboxItem("rememberData",
+				"Recordar mis Datos");
 		final LinkItem forgotData = new LinkItem("¿Olvido su Contraseña?");
-		
-		loginForm.setFields(username,password,submitButton,rememberData, forgotData);
-		
-		loginForm.draw();
-		
+
+		loginForm.setFields(username, password, submitButton, rememberData,
+				forgotData);
+
+		Window window = new Window();
+		window.setTitle("test");
+		window.setWidth(200);
+		window.setHeight(200);
+		window.addItem(loginForm);
+
+		window.show();
+
 	}
 
 }
