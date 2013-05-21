0e74beffe2dc903261ecdce53d79c2a25323dbd4
Angel Lacret <alacret@gmail.com>
2013-05-21 18:01:43 -0430

    mejoras login

--- webclient/src/main/java/org/fourgeeks/gha/webclient/client/Gha.java
+++ webclient/src/main/java/org/fourgeeks/gha/webclient/client/Gha.java
@@ -20,72 +20,75 @@ public class Gha implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		History.addValueChangeHandler(new ValueChangeHandler<String>() {
+			@Override
+			public void onValueChange(ValueChangeEvent<String> event) {
+				String historyToken = event.getValue();
+				if (historyToken == null)
+					return;
+
+				UIPlace place = UIPlacesFactory.createPlace(historyToken);
+				place.show();
+			}
+		});
+
 		final GWTUserServiceAsync service = GWT.create(GWTUserService.class);
 		service.isLogged(new AsyncCallback<Boolean>() {
 			@Override
 			public void onSuccess(Boolean result) {
-				History.newItem("login");
+				if (!result) {
+					String token = History.getToken();
+					if (token.equals("login"))
+						History.fireCurrentHistoryState();
+					else
+						History.newItem("login");
+				} else
+					History.fireCurrentHistoryState();
 			}
-		
+
 			@Override
 			public void onFailure(Throwable caught) {
-			// TODO Auto-generated method stub
-				
-			}
-		});
-		  
-		History.addValueChangeHandler(new ValueChangeHandler<String>() {
-			@Override
-			public void onValueChange(ValueChangeEvent<String> event) {
 				// TODO Auto-generated method stub
-				String historyToken = event.getValue();
-				if(historyToken!=null){
-					UIPlace place = UIPlacesFactory.createPlace(historyToken);
-					place.show();
-				}
-				//com.google.gwt.user.client.Window.alert("Result: "+historyToken.substring(historyToken.indexOf("=")+1));
+
 			}
 		});
+
 	}
-	
-	public void showLoginUI(boolean logged){
+
+	public void showLoginUI(boolean logged) {
 		/*
-		VLayout layout = new VLayout();
-		
-		if(!logged){
-			DynamicForm loginForm = new DynamicForm();
-			
-			Img logo = new Img();
-			Label title = new Label();
-			title.setContents("<h3>Iniciar Sesion</h3>");
-			Label ubicacion = new Label();
-			ubicacion.setTitle("Ubicado en:");
-			
-			TextItem username = new TextItem("username", "Nombre De Usuario");
-			PasswordItem password = new PasswordItem("password", "Contraseña");
-			SubmitItem submitButton = new SubmitItem("submitButton","Iniciar Sesion");
-			CheckboxItem rememberData = new CheckboxItem("rememberData", "Recordar mis Datos");
-			LinkItem forgotData = new LinkItem("¿Olvido su Contraseña?");
-			
-			loginForm.setFields(username, password, submitButton, rememberData,forgotData);
-			layout.addMember(logo);
-			layout.addMember(title);
-			layout.addMember(ubicacion);
-			layout.addMember(loginForm);
-			
-		}else{
-			layout.clear();
-			Label loggedText = new Label();
-			loggedText.setContents("<b>Ya existe un usuario loggeado en el sistema.</b>");
-			
-			layout.addMember(loggedText);
-		}
-		
-		RootPanel.get("main-content").add(layout);	
-		layout.draw();*/
-		if(!logged){
+		 * VLayout layout = new VLayout();
+		 * 
+		 * if(!logged){ DynamicForm loginForm = new DynamicForm();
+		 * 
+		 * Img logo = new Img(); Label title = new Label();
+		 * title.setContents("<h3>Iniciar Sesion</h3>"); Label ubicacion = new
+		 * Label(); ubicacion.setTitle("Ubicado en:");
+		 * 
+		 * TextItem username = new TextItem("username", "Nombre De Usuario");
+		 * PasswordItem password = new PasswordItem("password", "Contraseña");
+		 * SubmitItem submitButton = new
+		 * SubmitItem("submitButton","Iniciar Sesion"); CheckboxItem
+		 * rememberData = new CheckboxItem("rememberData",
+		 * "Recordar mis Datos"); LinkItem forgotData = new
+		 * LinkItem("¿Olvido su Contraseña?");
+		 * 
+		 * loginForm.setFields(username, password, submitButton,
+		 * rememberData,forgotData); layout.addMember(logo);
+		 * layout.addMember(title); layout.addMember(ubicacion);
+		 * layout.addMember(loginForm);
+		 * 
+		 * }else{ layout.clear(); Label loggedText = new Label();
+		 * loggedText.setContents
+		 * ("<b>Ya existe un usuario loggeado en el sistema.</b>");
+		 * 
+		 * layout.addMember(loggedText); }
+		 * 
+		 * RootPanel.get("main-content").add(layout); layout.draw();
+		 */
+		if (!logged) {
 			Window.alert("No esta loggeado.");
-		}else{
+		} else {
 			Window.alert("esta loggeado.");
 		}
 	}
--- webclient/src/main/java/org/fourgeeks/gha/webclient/client/UI/places/LoginPlace.java
+++ webclient/src/main/java/org/fourgeeks/gha/webclient/client/UI/places/LoginPlace.java
@@ -1,20 +1,42 @@
 package org.fourgeeks.gha.webclient.client.UI.places;
 
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.EventListener;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 
+public class LoginPlace implements UIPlace {
 
-
-public class LoginPlace implements UIPlace{
-	
 	public LoginPlace() {
 	}
 
-
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
-		HTML content = new HTML("<div class='login-panel'><div class='logo login-logo'></div><h1 class='login-titulo'>Iniciar Sesion</h1><div class='smallfont full'>Ubicado en:<span id='ubicacion'>Sotano enfermeria</span></div><form class='centered'><input type='text' value='Nombre de usuario'><br/><input type='password' value='Contraseña'> <br/><input type='submit' value='Iniciar Sesion' class='GHAButton'><div class='smallfont'><input type='checkbox'>Recordar mis datos</div><br/><a href='' class='smallfont'>¿Olvidaste tu contraseña?</a></form></div>");
+		Window.alert("login}");
+		StringBuilder html = new StringBuilder();
+		html.append("<div class='login-panel'>");
+		html.append("<div class='logo login-logo'></div>");
+		html.append("<h1 class='login-titulo'>Iniciar Sesion</h1>");
+		html.append("<div class='smallfont full'>Ubicado en:<span id='ubicacion'>Sotano enfermeria</span></div>");
+		html.append("<form class='centered'><input type='text' placeholder='Nombre de usuario'><br/><input type='password' placeholder='Contraseña'> <br/>");
+		html.append("<input id='login-button' type='button' value='Iniciar Sesion' class='GHAButton'>");
+		html.append("<div class='smallfont'><input type='checkbox'>Recordar mis datos</div><br/><a href='' class='smallfont'>¿Olvidaste tu contraseña?</a></form></div>");
+		HTML content = new HTML(html.toString());
 		RootPanel.get("main-content").add(content);
+
+		Element element = RootPanel.get("login-button").getElement();
+		DOM.sinkEvents(element, Event.ONCLICK);
+		DOM.setEventListener(element, new EventListener() {
+
+			@Override
+			public void onBrowserEvent(Event event) {
+				Window.alert("click");
+
+			}
+		});
+
 	}
 }
