1d81f692939754111c28c6da615a076c5ab767bb
Angel Lacret <alacret@gmail.com>
2013-06-11 16:29:51 -0430

    query a single sign on user, un par de tablas revisadas

--- domain/src/main/java/org/fourgeeks/gha/domain/ess/SingleSignOnUser.java
+++ domain/src/main/java/org/fourgeeks/gha/domain/ess/SingleSignOnUser.java
@@ -1,15 +1,13 @@
 package org.fourgeeks.gha.domain.ess;
 
-import java.util.Collection;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import org.fourgeeks.gha.domain.AbstractEntity;
+import org.fourgeeks.gha.domain.enu.UserLogonStatusEnum;
 import org.fourgeeks.gha.domain.mix.LegalEntity;
-import org.fourgeeks.gha.domain.mix.LegalEntityCredential;
 
 @Entity
 public class SingleSignOnUser extends AbstractEntity {
@@ -17,11 +15,92 @@ public class SingleSignOnUser extends AbstractEntity {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	@OneToOne
 	@JoinColumn(name = "legalEntityFk")
 	private LegalEntity legalEntity;
-	
-	@OneToMany(mappedBy = "singleSignOnUser")
-	private Collection<LegalEntityCredential> legalEntityCredentials;
+
+	private String password;
+	private String userName;
+	private UserLogonStatusEnum userLogonStatus;
+	@Column(/* nullable = false, */columnDefinition = "boolean NOT NULL DEFAULT false")
+	private boolean blocked;
+
+	/**
+	 * @return the legalEntity
+	 */
+	public LegalEntity getLegalEntity() {
+		return legalEntity;
+	}
+
+	/**
+	 * @param legalEntity
+	 *            the legalEntity to set
+	 */
+	public void setLegalEntity(LegalEntity legalEntity) {
+		this.legalEntity = legalEntity;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName
+	 *            the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the userLogonStatus
+	 */
+	public UserLogonStatusEnum getUserLogonStatus() {
+		return userLogonStatus;
+	}
+
+	/**
+	 * @param userLogonStatus
+	 *            the userLogonStatus to set
+	 */
+	public void setUserLogonStatus(UserLogonStatusEnum userLogonStatus) {
+		this.userLogonStatus = userLogonStatus;
+	}
+
+	/**
+	 * @return the blocked
+	 */
+	public boolean isBlocked() {
+		return blocked;
+	}
+
+	/**
+	 * @param blocked
+	 *            the blocked to set
+	 */
+	public void setBlocked(boolean blocked) {
+		this.blocked = blocked;
+	}
+
+	// @OneToMany(mappedBy = "singleSignOnUser")
+	// private Collection<LegalEntityCredential> legalEntityCredentials;
 }
--- domain/src/main/java/org/fourgeeks/gha/domain/mix/LegalEntity.java
+++ domain/src/main/java/org/fourgeeks/gha/domain/mix/LegalEntity.java
@@ -1,22 +1,16 @@
 package org.fourgeeks.gha.domain.mix;
 
 import java.sql.Timestamp;
-import java.util.Collection;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
 import org.fourgeeks.gha.domain.AbstractEntity;
-import org.fourgeeks.gha.domain.enu.LegalEntityTypeEnum;
-import org.fourgeeks.gha.domain.ess.SingleSignOnUser;
 
 /**
- * @author emiliot
- * Entidad legal aplicable a todas las personas Naturales y Jurídicas
- *
+ * @author emiliot Entidad legal aplicable a todas las personas Naturales y
+ *         Jurídicas
+ * 
  */
 @Entity
 public class LegalEntity extends AbstractEntity {
@@ -24,28 +18,36 @@ public class LegalEntity extends AbstractEntity {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	// TODO: certificar esta relacion
-	@OneToMany(mappedBy = "legalEntity")
-	private Collection <LegalEntityAddress> legalEntityAddresses;
-	
-	@OneToOne(mappedBy = "legalEntity")
-	private Institution institution;
-	
-	@OneToOne(mappedBy = "legalEntity")
-	private Citizen citizen;
-	
-	@OneToOne(mappedBy = "legalEntity")
-	private SingleSignOnUser signOnUser;
-	
-	/**Atributes*/
-	
-	private String Ueid; /** <PKEY> Universal Entity ID. Identificación Universal de Entidad Legal length =16 */
-	
-	@Enumerated(EnumType.STRING)
-	private LegalEntityTypeEnum legalEntityType; /** Tipo de Entidad Legal length =60 */
-	
-	private Timestamp dateCreated; /** Fecha y Hora de Creación length =22 */
+
+	// // TODO: certificar esta relacion
+	// @OneToMany(mappedBy = "legalEntity")
+	// private Collection <LegalEntityAddress> legalEntityAddresses;
+
+	// @OneToOne(mappedBy = "legalEntity")
+	// private Institution institution;
+	//
+	// @OneToOne(mappedBy = "legalEntity")
+	// private Citizen citizen;
+	//
+	// @OneToOne(mappedBy = "legalEntity")
+	// private SingleSignOnUser signOnUser;
+
+	/** Atributes */
+
+	private String Ueid;
+	/**
+	 * <PKEY> Universal Entity ID. Identificación Universal de Entidad Legal
+	 * length =16
+	 */
+
+	// @Enumerated(EnumType.STRING)
+	// private LegalEntityTypeEnum legalEntityType;
+	// /** Tipo de Entidad Legal length =60 */
+	@Column(/* nullable = false, */columnDefinition = "timestamp without time zone NOT NULL DEFAULT NOW()")
+	private Timestamp dateCreated = new Timestamp(
+			new java.util.Date().getTime());
+
+	/** Fecha y Hora de Creación length =22 */
 
 	/**
 	 * 
@@ -53,5 +55,35 @@ public class LegalEntity extends AbstractEntity {
 	public LegalEntity() {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the ueid
+	 */
+	public String getUeid() {
+		return Ueid;
+	}
+
+	/**
+	 * @param ueid
+	 *            the ueid to set
+	 */
+	public void setUeid(String ueid) {
+		Ueid = ueid;
+	}
+
+	/**
+	 * @return the dateCreated
+	 */
+	public Timestamp getDateCreated() {
+		return dateCreated;
+	}
+
+	/**
+	 * @param dateCreated
+	 *            the dateCreated to set
+	 */
+	public void setDateCreated(Timestamp dateCreated) {
+		this.dateCreated = dateCreated;
 	}
 }
