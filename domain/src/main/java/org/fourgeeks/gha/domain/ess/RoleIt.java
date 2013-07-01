6ab35ee1addaa7d43d6c48d38179cd75fe0ab86c
Emilio Tirado <etirado@stcsolutions.com.ve>
2013-07-01 12:04:14 -0430

    actualizacion en la entidad roleIt para contemplar roles base

--- domain/src/main/java/org/fourgeeks/gha/domain/ess/RoleIt.java
+++ domain/src/main/java/org/fourgeeks/gha/domain/ess/RoleIt.java
@@ -3,12 +3,15 @@ package org.fourgeeks.gha.domain.ess;
 import java.util.Collection;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import org.fourgeeks.gha.domain.AbstractEntity;
 import org.fourgeeks.gha.domain.gar.Bpu;
 import org.fourgeeks.gha.domain.gar.JobPosition;
+import org.fourgeeks.gha.domain.gar.Obu;
 
 @Entity
 public class RoleIt extends AbstractEntity{
@@ -32,5 +35,86 @@ public class RoleIt extends AbstractEntity{
 	
 	@ManyToMany(mappedBy = "itRoles")
 	private Collection <AuthorizationGroup> authGroups;
+	
+	@ManyToOne
+	@JoinColumn(name = "baseRoleFk")
+	private BaseRole baseRole;
+	
+	@ManyToOne
+	@JoinColumn(name = "obuFk")
+	private Obu obu;
+
+	/**
+	 * 
+	 */
+	public RoleIt() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param baseRole
+	 * @param obu
+	 */
+	public RoleIt(BaseRole baseRole, Obu obu) {
+		this.baseRole = baseRole;
+		this.obu = obu;
+	}
+
+	public Collection<Bpu> getBpus() {
+		return bpus;
+	}
+
+	public Collection<JobPosition> getJobPositions() {
+		return jobPositions;
+	}
+
+	public Collection<ViewFunction> getViewFunctions() {
+		return viewFunctions;
+	}
+
+	public Collection<WorkingArea> getWorkingAreas() {
+		return workingAreas;
+	}
+
+	public Collection<AuthorizationGroup> getAuthGroups() {
+		return authGroups;
+	}
+
+	public BaseRole getBaseRole() {
+		return baseRole;
+	}
+
+	public Obu getObu() {
+		return obu;
+	}
+
+	public void setBpus(Collection<Bpu> bpus) {
+		this.bpus = bpus;
+	}
+
+	public void setJobPositions(Collection<JobPosition> jobPositions) {
+		this.jobPositions = jobPositions;
+	}
+
+	public void setViewFunctions(Collection<ViewFunction> viewFunctions) {
+		this.viewFunctions = viewFunctions;
+	}
+
+	public void setWorkingAreas(Collection<WorkingArea> workingAreas) {
+		this.workingAreas = workingAreas;
+	}
+
+	public void setAuthGroups(Collection<AuthorizationGroup> authGroups) {
+		this.authGroups = authGroups;
+	}
+
+	public void setBaseRole(BaseRole baseRole) {
+		this.baseRole = baseRole;
+	}
+
+	public void setObu(Obu obu) {
+		this.obu = obu;
+	}
+	
 	
 }
