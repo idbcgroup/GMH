583c02936e2025a101947f8d103ca22ade7a61e7
Angel Lacret <alacret@gmail.com>
2013-07-11 16:35:29 -0430

    baseRole service

---
+++ webclient/src/main/java/org/fourgeeks/gha/webclient/server/baserole/GWTBaseRoleServiceImpl.java
@@ -0,0 +1,31 @@
+package org.fourgeeks.gha.webclient.server.baserole;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+
+import org.fourgeeks.gha.domain.ess.BaseRole;
+import org.fourgeeks.gha.domain.exceptions.EJBException;
+import org.fourgeeks.gha.ejb.ess.BaseRoleServiceRemote;
+import org.fourgeeks.gha.webclient.client.baserole.GWTBaseRoleService;
+
+/**
+ * @author alacret
+ * 
+ */
+public class GWTBaseRoleServiceImpl implements GWTBaseRoleService {
+
+	@EJB(name = "gmh.BaseRoleService")
+	BaseRoleServiceRemote service;
+
+	@Override
+	public List<BaseRole> getAll() throws EJBException {
+		return service.getAll();
+	}
+
+	@Override
+	public List<BaseRole> getAll(int offset, int size) throws EJBException {
+		// TODO
+		return service.getAll();
+	}
+}
