ecd21b33c8c2d06cf2a4913eeba6504a084731b3
Alejandro Rodríguez <arodriguez@4geeks.co>
2013-07-23 20:54:34 -0430

    Creado EiaTypeComponentRecord

---
+++ webclient/src/main/java/org/fourgeeks/gha/webclient/client/eiatype/component/EiaTypeComponentRecord.java
@@ -0,0 +1,90 @@
+package org.fourgeeks.gha.webclient.client.eiatype.component;
+
+import org.fourgeeks.gha.domain.gmh.EiaType;
+import org.fourgeeks.gha.domain.gmh.EiaTypeComponent;
+import org.fourgeeks.gha.webclient.client.UI.GHAGridRecord;
+
+public class EiaTypeComponentRecord
+		extends GHAGridRecord<EiaTypeComponent> {
+
+	private EiaTypeComponent eiaTypeComponent;
+	
+	public EiaTypeComponentRecord(EiaTypeComponent eiaTypeComponent) {
+		setEiaTypeComponent(eiaTypeComponent);
+	}
+	
+	public void setEiaTypeComponent(EiaTypeComponent eiaTypeComponent) {
+		this.eiaTypeComponent = eiaTypeComponent;
+		EiaType eiaType = this.eiaTypeComponent.getEiaType();
+		setCode(eiaType.getCode());
+		setName(eiaType.getName());
+		if (eiaType.getBrand() != null)
+			setBrand(eiaType.getBrand().getName());
+		setModel(eiaType.getModel());
+		if (eiaType.getManufacturer() != null)
+			setManufacturer(eiaType.getManufacturer().getName());
+		setRequired(String.valueOf(this.eiaTypeComponent.isComponentRequired()));
+		setReplaceable(String.valueOf(this.eiaTypeComponent.isComponentReplaceable()));
+	}
+	public void setName(String name) {
+		setAttribute("name", name);
+	}
+
+	public String getName() {
+		return getAttributeAsString("name");
+	}
+
+	public void setCode(String code) {
+		setAttribute("code", code);
+	}
+
+	public String getCode() {
+		return getAttributeAsString("code");
+	}
+
+	public void setBrand(String brand) {
+		setAttribute("brand", brand);
+	}
+
+	public String getBrand() {
+		return getAttributeAsString("brand");
+	}
+
+	public void setModel(String model) {
+		setAttribute("model", model);
+	}
+
+	public String getModel() {
+		return getAttributeAsString("model");
+	}
+
+	public void setManufacturer(String manufacturer) {
+		setAttribute("manufacturer", manufacturer);
+	}
+
+	public String getManufacturer() {
+		return getAttributeAsString("manufacturer");
+	}
+
+	public void setRequired(String required) {
+		setAttribute("required", required);
+	}
+	
+	public String getRequired() {
+		return getAttributeAsString("required");
+	}
+
+	public void setReplaceable(String replaceable) {
+		setAttribute("replaceable", replaceable);
+	}
+	
+	public String getReplaceable() {
+		return getAttributeAsString("replaceable");
+	}
+	
+	@Override
+	public EiaTypeComponent toEntity() {
+		return eiaTypeComponent;
+	}
+
+}
