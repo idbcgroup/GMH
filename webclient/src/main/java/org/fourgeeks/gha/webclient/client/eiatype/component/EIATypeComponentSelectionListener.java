df77627cf4f6d5658f1e1a5716d02a155bf78d01
Alejandro Rodríguez <arodriguez@4geeks.co>
2013-07-26 10:30:39 -0430

    Añadido EIATypeComponentAddForm

---
+++ webclient/src/main/java/org/fourgeeks/gha/webclient/client/eiatype/component/EIATypeComponentSelectionListener.java
@@ -0,0 +1,9 @@
+package org.fourgeeks.gha.webclient.client.eiatype.component;
+
+import org.fourgeeks.gha.domain.gmh.EiaTypeComponent;
+
+public interface EIATypeComponentSelectionListener {
+
+	public void select(EiaTypeComponent eiaTypeComponent);
+	
+}
