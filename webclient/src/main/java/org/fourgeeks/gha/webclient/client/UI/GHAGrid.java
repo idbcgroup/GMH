9f86efb2698b2e3b13b8dabcc9239765dda62d52
Angel Lacret <alacret@gmail.com>
2013-08-05 15:34:32 -0430

    some fixxes

---
+++ webclient/src/main/java/org/fourgeeks/gha/webclient/client/UI/GHAGrid.java
@@ -0,0 +1,14 @@
+package org.fourgeeks.gha.webclient.client.UI;
+
+import com.smartgwt.client.widgets.grid.ListGrid;
+
+public class GHAGrid<T extends GHAGridRecord> extends ListGrid {
+
+	public GHAGrid() {
+
+	}
+
+	public T getSelectedRecord() {
+		return getSelectedRecord();
+	}
+}
--- webclient/src/main/java/org/fourgeeks/gha/webclient/client/UI/GHASubTab.java
+++ webclient/src/main/java/org/fourgeeks/gha/webclient/client/UI/GHASubTab.java
@@ -5,17 +5,21 @@ import java.util.List;
 
 import com.smartgwt.client.widgets.tab.Tab;
 
+/**
+ * @author alacret Subclase que representa un subtab de las pantallas
+ * 
+ */
 public class GHASubTab extends Tab implements GHAClosable, GHAHideable {
 	private List<GHAClosable> closables = new ArrayList<GHAClosable>();
 	private List<GHAHideable> hideables = new ArrayList<GHAHideable>();
-	
-	public GHASubTab(String title, GHATab tab){
+
+	public GHASubTab(String title, GHATab tab) {
 		tab.addGHAHideableHandler(this);
 		tab.addGHAClosableHandler(this);
 		setTitle(title);
 		setPaneMargin(0);
 	}
-	
+
 	@Override
 	public void close() {
 		for (GHAClosable closable : closables)
