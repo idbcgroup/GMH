b562227829aad0a98b04d794d159af958a4973a1
Angel Lacret <alacret@gmail.com>
2013-08-12 15:58:12 -0430

    some fixes

--- domain/src/main/java/org/fourgeeks/gha/domain/gmh/EiaSpare.java
+++ domain/src/main/java/org/fourgeeks/gha/domain/gmh/EiaSpare.java
@@ -6,23 +6,32 @@ package org.fourgeeks.gha.domain.gmh;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import org.fourgeeks.gha.domain.AbstractEntity;
 
 /**
- * @author emiliot
- *
+ * @author alacret
+ * 
  */
 
 @Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "eiaFk",
+		"spareEiaFk" }))
 public class EiaSpare extends AbstractEntity {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	@ManyToOne
-	@JoinColumn(name = "parentEiaFk")
-	private Eia parentEia;
+	@JoinColumn(name = "eiaFk")
+	private Eia eia;
+
+	@OneToOne
+	@JoinColumn(name = "spareEiaFk")
+	private Eia spareEia;
 }
--- webclient/src/main/java/org/fourgeeks/gha/webclient/client/eia/component/EIAComponentGrid.java
+++ webclient/src/main/java/org/fourgeeks/gha/webclient/client/eia/component/EIAComponentGrid.java
@@ -1,65 +1,67 @@
 package org.fourgeeks.gha.webclient.client.eia.component;
 
+import org.fourgeeks.gha.domain.gmh.Eia;
 import org.fourgeeks.gha.webclient.client.UI.GHAUiHelper;
+import org.fourgeeks.gha.webclient.client.UI.GhaGrid;
 
-import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.smartgwt.client.types.Alignment;
-import com.smartgwt.client.widgets.grid.ListGrid;
 import com.smartgwt.client.widgets.grid.ListGridField;
 
-public class EIAComponentGrid extends ListGrid implements ResizeHandler{
+public class EIAComponentGrid extends GhaGrid<Eia> implements ResizeHandler {
 
 	public EIAComponentGrid() {
-		GHAUiHelper.addGHAResizeHandler(this);
-		
 		setWidth100();
 		setHeight(GHAUiHelper.getGridSize(30));
 		setEmptyMessage("No existen partes de equipos para mostrar.");
 
 		setAlternateRecordStyles(false);
 		setCanResizeFields(false);
-//		setShowFilterEditor(true);
+		// setShowFilterEditor(true);
 
 		ListGridField idGridField = new ListGridField("id", "No");
 		idGridField.setAlign(Alignment.CENTER);
 
-		ListGridField partnumGridField = new ListGridField("nParte", "No Parte EIA");
+		ListGridField partnumGridField = new ListGridField("nParte",
+				"No Parte EIA");
 		partnumGridField.setAlign(Alignment.CENTER);
-		
-		ListGridField nameGridField = new ListGridField("nameParte", "Nombre Parte");
+
+		ListGridField nameGridField = new ListGridField("nameParte",
+				"Nombre Parte");
 		nameGridField.setAlign(Alignment.CENTER);
-		
-		ListGridField usoparteGridField = new ListGridField("usoParte", "Uso Parte");
+
+		ListGridField usoparteGridField = new ListGridField("usoParte",
+				"Uso Parte");
 		usoparteGridField.setAlign(Alignment.CENTER);
-		
+
 		ListGridField typeGridField = new ListGridField("type", "Tipo Parte");
 		typeGridField.setAlign(Alignment.CENTER);
 
 		ListGridField requiredGridField = new ListGridField("req", "Requerido");
 		requiredGridField.setAlign(Alignment.CENTER);
-		
+
 		ListGridField sustGridField = new ListGridField("sust", "Sustituible");
 		sustGridField.setAlign(Alignment.CENTER);
 
-		ListGridField eiaCodeGridField = new ListGridField("eiaCode", "Cod. Equipo");
+		ListGridField eiaCodeGridField = new ListGridField("eiaCode",
+				"Cod. Equipo");
 		eiaCodeGridField.setAlign(Alignment.CENTER);
-		
-		ListGridField eiaSerialGridField = new ListGridField("eiaSerial", "No. Serial Equipo");
+
+		ListGridField eiaSerialGridField = new ListGridField("eiaSerial",
+				"No. Serial Equipo");
 		eiaSerialGridField.setAlign(Alignment.CENTER);
-		
-		ListGridField facilityGridField = new ListGridField("facility", "Ubicación");
+
+		ListGridField facilityGridField = new ListGridField("facility",
+				"Ubicación");
 		facilityGridField.setAlign(Alignment.CENTER);
-		
-		ListGridField statusGridField = new ListGridField("status", "Estado Equipo");
+
+		ListGridField statusGridField = new ListGridField("status",
+				"Estado Equipo");
 		statusGridField.setAlign(Alignment.CENTER);
 
-		setFields(idGridField, partnumGridField, nameGridField, usoparteGridField, typeGridField, requiredGridField, sustGridField, eiaCodeGridField, eiaSerialGridField, facilityGridField);
+		setFields(idGridField, partnumGridField, nameGridField,
+				usoparteGridField, typeGridField, requiredGridField,
+				sustGridField, eiaCodeGridField, eiaSerialGridField,
+				facilityGridField);
 	}
-
-	@Override
-	public void onResize(ResizeEvent event) {
-		setHeight(GHAUiHelper.getGridSize(30));
-	}
-
 }
