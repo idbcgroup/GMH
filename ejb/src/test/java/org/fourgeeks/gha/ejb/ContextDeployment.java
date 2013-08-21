e5803c93f8ed8c7ddb13662b59c8a12794648785
Angel Lacret <alacret@gmail.com>
2013-08-21 17:31:34 -0430

    buggy

--- ejb/src/test/java/org/fourgeeks/gha/ejb/ContextDeployment.java
+++ ejb/src/test/java/org/fourgeeks/gha/ejb/ContextDeployment.java
@@ -4,8 +4,6 @@ import java.util.Properties;
 
 import javax.ejb.embeddable.EJBContainer;
 import javax.naming.Context;
-import javax.persistence.EntityManager;
-
 
 /**
  * @author alacret
@@ -14,10 +12,10 @@ import javax.persistence.EntityManager;
 public class ContextDeployment {
 
 	private static Context context;
-	 private static EntityManager em;
+	// private static EntityManager em;
 
 	static {
-		 final Properties p = new Properties();
+		final Properties p = new Properties();
 		p.put(Context.INITIAL_CONTEXT_FACTORY,
 				"org.apache.openejb.client.LocalInitialContextFactory");
 		p.put("gha", "new://Resource?type=DataSource");
@@ -28,33 +26,34 @@ public class ContextDeployment {
 		p.put("gha.password", "postgres");
 		context = EJBContainer.createEJBContainer(p).getContext();
 
-//		em = Persistence.createEntityManagerFactory("gha").createEntityManager();
+		// em =
+		// Persistence.createEntityManagerFactory("gha").createEntityManager();
 
 	}
 
-	public ContextDeployment() {
-		final Properties p = new Properties();
-		p.put(Context.INITIAL_CONTEXT_FACTORY,
-				"org.apache.openejb.client.LocalInitialContextFactory");
-		p.put("gha", "new://Resource?type=DataSource");
-		// p.put("gha.hibernate.hbm2ddl.auto", "create");
-		p.put("gha.JdbcDriver", "org.postgresql.Driver");
-		p.put("gha.JdbcUrl", "jdbc:postgresql://localhost:5432/gha");
-		p.put("gha.username", "postgres");
-		p.put("gha.password", "postgres");
-		context = EJBContainer.createEJBContainer(p).getContext();
-
-//		em = Persistence.createEntityManagerFactory("gha")
-//				.createEntityManager();
-
-	}
+	// public ContextDeployment() {
+	// final Properties p = new Properties();
+	// p.put(Context.INITIAL_CONTEXT_FACTORY,
+	// "org.apache.openejb.client.LocalInitialContextFactory");
+	// p.put("gha", "new://Resource?type=DataSource");
+	// // p.put("gha.hibernate.hbm2ddl.auto", "create");
+	// p.put("gha.JdbcDriver", "org.postgresql.Driver");
+	// p.put("gha.JdbcUrl", "jdbc:postgresql://localhost:5432/gha");
+	// p.put("gha.username", "postgres");
+	// p.put("gha.password", "postgres");
+	// context = EJBContainer.createEJBContainer(p).getContext();
+	//
+	// // em = Persistence.createEntityManagerFactory("gha")
+	// // .createEntityManager();
+	//
+	// }
 
 	public static Context getContext() {
 		return context;
 	}
 
-	 public static EntityManager getEntityManager() {
-		 return em;
-	 }
+	// public static EntityManager getEntityManager() {
+	// return em;
+	// }
 
 }
