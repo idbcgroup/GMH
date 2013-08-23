3b5fd73a30e782279e7a87586530fff3c55bead6
Emilio Tirado <etirado@stcsolutions.com.ve>
2013-08-23 15:26:18 -0430

    entidad para el manyToMany de resources con protocolActiviites

---
+++ ejb/src/main/java/org/fourgeeks/gha/ejb/gmh/ProtocolActivityResources.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package org.fourgeeks.gha.ejb.gmh;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import org.fourgeeks.gha.domain.AbstractEntity;
+import org.fourgeeks.gha.domain.gmh.ProtocolActivity;
+import org.fourgeeks.gha.domain.gmh.Resource;
+
+/**
+ * @author emiliot
+ * ManyToMany between Resources and ProtocolActivities
+ *
+ */
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "protocolActivityFk", "resourceFk" }))
+public class ProtocolActivityResources extends AbstractEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	@JoinColumn(name = "protocolActivityFk", nullable = false)
+	private ProtocolActivity protocolActivity;
+	
+	@ManyToOne
+	@JoinColumn(name = "resourceFk", nullable = false)
+	private Resource resource;
+
+	/**
+	 * 
+	 */
+	public ProtocolActivityResources() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProtocolActivity getProtocolActivity() {
+		return protocolActivity;
+	}
+
+	public Resource getResource() {
+		return resource;
+	}
+
+	public void setProtocolActivity(ProtocolActivity protocolActivity) {
+		this.protocolActivity = protocolActivity;
+	}
+
+	public void setResource(Resource resource) {
+		this.resource = resource;
+	}
+	
+	
+}
