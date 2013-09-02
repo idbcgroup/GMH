df633d3ea59fa6af990a0610c98999c3e1592d8e
Emilio Tirado <etirado@stcsolutions.com.ve>
2013-09-02 15:56:19 -0430

    categories entities

---
+++ domain/src/main/java/org/fourgeeks/gha/domain/gar/FacilityCategory.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.fourgeeks.gha.domain.gar;
+
+import javax.persistence.Entity;
+
+import org.fourgeeks.gha.domain.AbstractEntity;
+
+/**
+ * @author emiliot
+ *
+ */
+@Entity
+public class FacilityCategory extends AbstractEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ domain/src/main/java/org/fourgeeks/gha/domain/gar/JobCategory.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.fourgeeks.gha.domain.gar;
+
+import javax.persistence.Entity;
+
+import org.fourgeeks.gha.domain.AbstractEntity;
+
+/**
+ * @author emiliot
+ *
+ */
+@Entity
+public class JobCategory extends AbstractEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
