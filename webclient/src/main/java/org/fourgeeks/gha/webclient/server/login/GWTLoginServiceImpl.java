8304d644f1abe4004ade94453bb44b9d63c984aa
Angel Lacret <alacret@gmail.com>
2013-09-17 10:24:43 -0430

    bug fix

--- webclient/src/main/java/org/fourgeeks/gha/webclient/server/login/GWTLoginServiceImpl.java
+++ webclient/src/main/java/org/fourgeeks/gha/webclient/server/login/GWTLoginServiceImpl.java
@@ -2,14 +2,12 @@ package org.fourgeeks.gha.webclient.server.login;
 
 import java.util.logging.Logger;
 
-import javax.ejb.EJB;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.fourgeeks.gha.domain.ess.SSOUser;
 import org.fourgeeks.gha.domain.gar.Bpu;
-import org.fourgeeks.gha.ejb.ess.SSOUserServiceRemote;
 import org.fourgeeks.gha.webclient.client.login.GWTLoginService;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
@@ -18,15 +16,16 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
  * @author alacret
  * 
  */
+@WebServlet(urlPatterns = { "/webclient/login" })
 public class GWTLoginServiceImpl extends RemoteServiceServlet implements
 		GWTLoginService {
 
 	private static final long serialVersionUID = 1L;
 	private final static Logger logger = Logger
 			.getLogger(GWTLoginServiceImpl.class.getName());
-	
-	@EJB(name = "ess.SSOUser")
-	SSOUserServiceRemote ssoUserService;
+
+	// @EJB(name = "ess.SSOUserService")
+	// SSOUserServiceRemote ssoUserService;
 
 	/**
 	 * @return true if there is a user logged in
@@ -55,17 +54,17 @@ public class GWTLoginServiceImpl extends RemoteServiceServlet implements
 		try {
 			request.login(user, password);
 			// TODO : Aqui se debe hookear el guardado del log de login
-			
-			//get the bpu for the authenticated user
-			SSOUser ssoUser = ssoUserService.findByUsername(user);
-			if(ssoUser.isBlocked()){
-				//TODO: Usuario bloqueado intentando acceder a la aplicacion
-			}else{
-				//usuario valido
-				return ssoUser.getBpu();
-			}
-			
-			return null;
+
+			// get the bpu for the authenticated user
+			// SSOUser ssoUser = ssoUserService.findByUsername(user);
+			// if (ssoUser.isBlocked()) {
+			// // TODO: Usuario bloqueado intentando acceder a la aplicacion
+			// } else {
+			// // usuario valido
+			// return ssoUser.getBpu();
+			// }
+
+			return new Bpu();
 		} catch (ServletException e) {
 			System.out.println("Error iniciando sesión " + e.getMessage());
 			// TODO : Aqui se debe hookear el guardado del log de login
