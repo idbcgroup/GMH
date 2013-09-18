f07c4ec6664753f92af6b7e8328183db021cae7e
Angel Lacret <alacret@gmail.com>
2013-09-18 11:52:17 -0430

    user selection listener and producer

--- webclient/src/main/java/org/fourgeeks/gha/webclient/client/user/information/UserInformationSubTab.java
+++ webclient/src/main/java/org/fourgeeks/gha/webclient/client/user/information/UserInformationSubTab.java
@@ -1,19 +1,34 @@
 package org.fourgeeks.gha.webclient.client.user.information;
 
+import org.fourgeeks.gha.domain.ess.SSOUser;
 import org.fourgeeks.gha.webclient.client.UI.superclasses.GHASubTab;
+import org.fourgeeks.gha.webclient.client.user.UserSelectionListener;
 import org.fourgeeks.gha.webclient.client.user.UserTab;
 
-public class UserInformationSubTab extends GHASubTab {
+/**
+ * @author alacret
+ * 
+ */
+public class UserInformationSubTab extends GHASubTab implements
+		UserSelectionListener {
 
 	private UserInformationFormPanel form;
 
+	/**
+	 * @param tab
+	 */
 	public UserInformationSubTab(UserTab tab) {
 		super("Información", tab);
-		
+		tab.addUserSelectionListener(this);
 		form = new UserInformationFormPanel(tab);
 		addGHAClosableHandler(form);
 		addGHAHideableHandler(form);
-		
 		setPane(form);
+	}
+
+	@Override
+	public void select(SSOUser ssoUser) {
+		// TODO Auto-generated method stub
+
 	}
 }
