825794947009a60ea584108459bcea0ddafbc741
naramirez <naramirez@4geeks.com.ve>
2013-09-21 12:02:15 -0430

    implementacion completa de ReportBrandServlet

--- webclient/src/main/java/org/fourgeeks/gha/webclient/server/ReportBrandServelt.java
+++ webclient/src/main/java/org/fourgeeks/gha/webclient/server/ReportBrandServelt.java
@@ -1,7 +1,9 @@
 package org.fourgeeks.gha.webclient.server;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletConfig;
@@ -11,6 +13,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
 import org.fourgeeks.gha.domain.exceptions.EJBException;
 import org.fourgeeks.gha.domain.gmh.Brand;
 import org.fourgeeks.gha.ejb.gmh.BrandServiceRemote;
@@ -18,23 +26,58 @@ import org.fourgeeks.gha.ejb.gmh.BrandServiceRemote;
 @WebServlet(urlPatterns = { "/webclient/reportBrand" })
 public class ReportBrandServelt extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String REPORT_FILE_DIR = "../reportes/prueba.jasper";
 
 	@EJB(name = "gmh.BrandService", beanInterface = BrandServiceRemote.class)
 	BrandServiceRemote service;
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.GenericServlet#init(javax.servlet.ServletConfig)
+	 */
 	public void init(ServletConfig servletConfig) throws ServletException {
 		super.init(servletConfig);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest
+	 * , javax.servlet.http.HttpServletResponse)
+	 */
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 
 		try {
+			Map<String, Object> paramsReport = new HashMap<String, Object>();
+
 			List<Brand> brands = service.getAll();
+			JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(
+					brands);
+
+			JasperPrint fillReport = JasperFillManager.fillReport(
+					REPORT_FILE_DIR, paramsReport, dataSource);
+
+			exportAsPDF(resp, fillReport);
 
 		} catch (EJBException e) {
 			e.printStackTrace();
+		} catch (JRException e) {
+			e.printStackTrace();
 		}
+	}
+
+	private void exportAsPDF(HttpServletResponse response, JasperPrint impresion)
+			throws JRException, IOException {
+
+		response.setContentType("application/pdf");
+		response.addHeader("Content-Disposition",
+				"inline; filename=brandReport.pdf");
+
+		JasperExportManager.exportReportToPdfStream(impresion,
+				response.getOutputStream());
 	}
 }
