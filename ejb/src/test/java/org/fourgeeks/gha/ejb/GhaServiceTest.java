2dd9576e552471a0c44383759fba48256ace87e3
Viviana Torres Guerra <vtorres@stcsolutions.com.ve>
2013-09-26 15:16:45 +0100

    RoleServiceTest agregado. La prueba assertEquals(entity, service.find(entity.getId())) no funciona; donde entity es el valor obtenido luego de hacer save usando RoleService

--- ejb/src/test/java/org/fourgeeks/gha/ejb/GhaServiceTest.java
+++ ejb/src/test/java/org/fourgeeks/gha/ejb/GhaServiceTest.java
@@ -7,27 +7,23 @@ import org.fourgeeks.gha.domain.HasKey;
 import org.fourgeeks.gha.domain.enu.EiaMobilityEnum;
 import org.fourgeeks.gha.domain.enu.LocationLevelEnum;
 import org.fourgeeks.gha.domain.ess.BpuFunction;
-import org.fourgeeks.gha.domain.ess.Function;
 import org.fourgeeks.gha.domain.ess.InstanceLogon;
 import org.fourgeeks.gha.domain.ess.ItSystem;
+import org.fourgeeks.gha.domain.ess.Role;
 import org.fourgeeks.gha.domain.ess.WorkingArea;
-import org.fourgeeks.gha.domain.exceptions.GHAEJBException;
 import org.fourgeeks.gha.domain.gar.Bpu;
 import org.fourgeeks.gha.domain.gar.Facility;
 import org.fourgeeks.gha.domain.glm.ExternalProvider;
 import org.fourgeeks.gha.domain.glm.Material;
 import org.fourgeeks.gha.domain.gmh.Brand;
 import org.fourgeeks.gha.domain.gmh.ServiceResource;
-import org.fourgeeks.gha.domain.logs.LogonLog;
 import org.fourgeeks.gha.domain.mix.Bpa;
 import org.fourgeeks.gha.domain.mix.LegalEntity;
-import org.fourgeeks.gha.domain.msg.Message;
-import org.fourgeeks.gha.ejb.ess.FunctionServiceRemote;
 import org.fourgeeks.gha.ejb.ess.InstanceLogonService;
+import org.fourgeeks.gha.ejb.ess.RoleService;
 import org.fourgeeks.gha.ejb.gar.BpuFunctionService;
 import org.fourgeeks.gha.ejb.gar.BpuFunctionServiceRemote;
 import org.fourgeeks.gha.ejb.gmh.BrandService;
-import org.fourgeeks.gha.ejb.log.LogonLogServiceRemote;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -35,7 +31,7 @@ import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 
 /**
- * @author alacret
+ * @author alacret, vivi.torresg
  * 
  */
 public class GhaServiceTest {
@@ -49,38 +45,30 @@ public class GhaServiceTest {
 	public static Archive<?> createDeployment() {
 		return ShrinkWrap
 				.create(WebArchive.class, "test.war")
-				.addPackage(Brand.class.getPackage())
-				.addPackage(GhaServiceTest.class.getPackage())
-				.addPackage(HasKey.class.getPackage())
 				.addPackage(AbstractEntity.class.getPackage())
-				.addPackage(WorkingArea.class.getPackage())
-				.addPackage(EiaMobilityEnum.class.getPackage())
-				.addPackage(FunctionServiceRemote.class.getPackage())
-				.addPackage(ExternalProvider.class.getPackage())
-				.addPackage(Facility.class.getPackage())
-				.addPackage(Function.class.getPackage())
-				.addPackage(LegalEntity.class.getPackage())
-				.addPackage(BpuFunction.class.getPackage())
-				.addPackage(GHAEJBException.class.getPackage())
-				.addPackage(javax.ejb.EJBException.class.getPackage())
-				.addPackage(LogonLog.class.getPackage())
-				.addPackage(LogonLogServiceRemote.class.getPackage())
-				.addPackage(Message.class.getPackage())
-				.addPackage(ItSystem.class.getPackage())
-				.addPackage(AbstractEntity.class.getPackage())
-				.addPackage(Bpu.class.getPackage())
 				.addPackage(Bpa.class.getPackage())
-				.addPackage(LocationLevelEnum.class.getPackage())
-				.addPackage(ServiceResource.class.getPackage())
-				.addPackage(Material.class.getPackage())
-				.addPackage(BpuFunctionServiceRemote.class.getPackage())
+				.addPackage(Bpu.class.getPackage())
+				.addPackage(BpuFunction.class.getPackage())
 				.addPackage(BpuFunctionService.class.getPackage())
-				.addPackage(LegalEntity.class.getPackage())
+				.addPackage(BpuFunctionServiceRemote.class.getPackage())
 				.addPackage(Brand.class.getPackage())
 				.addPackage(BrandService.class.getPackage())
+				.addPackage(EiaMobilityEnum.class.getPackage())
+				.addPackage(EJBException.class.getPackage())
+				.addPackage(ExternalProvider.class.getPackage())
+				.addPackage(Facility.class.getPackage())
+				.addPackage(GhaServiceTest.class.getPackage())
+				.addPackage(HasKey.class.getPackage())
+				.addPackage(ItSystem.class.getPackage())
 				.addPackage(InstanceLogon.class.getPackage())
 				.addPackage(InstanceLogonService.class.getPackage())
-				.addPackage(EJBException.class.getPackage())
+				.addPackage(LegalEntity.class.getPackage())
+				.addPackage(LocationLevelEnum.class.getPackage())
+				.addPackage(Material.class.getPackage())
+				.addPackage(Role.class.getPackage())
+				.addPackage(RoleService.class.getPackage())
+				.addPackage(ServiceResource.class.getPackage())
+				.addPackage(WorkingArea.class.getPackage())
 				.addAsResource("test-persistence.xml",
 						"META-INF/persistence.xml")
 				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
--- ejb/src/test/java/org/fourgeeks/gha/ejb/ess/RoleServiceTest.java
+++ ejb/src/test/java/org/fourgeeks/gha/ejb/ess/RoleServiceTest.java
@@ -13,9 +13,8 @@ import javax.transaction.UserTransaction;
 
 import junit.framework.Assert;
 
-import org.fourgeeks.gha.domain.ess.InstanceLogon;
+import org.fourgeeks.gha.domain.ess.Role;
 import org.fourgeeks.gha.domain.exceptions.GHAEJBException;
-import org.fourgeeks.gha.domain.mix.Bpa;
 import org.fourgeeks.gha.ejb.GhaServiceTest;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Test;
@@ -26,12 +25,12 @@ import org.junit.runner.RunWith;
  * 
  */
 @RunWith(Arquillian.class)
-public class InstanceLogonServiceTest extends GhaServiceTest {
+public class RoleServiceTest extends GhaServiceTest {
 	@PersistenceContext
 	EntityManager em;
 
-	@EJB(name = "ess.InstanceLogonService")
-	InstanceLogonServiceRemote service;
+	@EJB(name = "ess.RoleService")
+	RoleServiceRemote service;
 
 	@Inject
 	UserTransaction ux;
@@ -47,26 +46,24 @@ public class InstanceLogonServiceTest extends GhaServiceTest {
 		ux.begin();
 		em.joinTransaction();
 
-		InstanceLogon entity = new InstanceLogon();
-
+		Role entity = new Role();
+		entity.setName("Role test name");
 		entity = service.save(entity);
 
 		Assert.assertNotNull(entity);
-		// Assert.assertEquals(entity, service.find(entity)); //TODO el método
-		// del servicio siempre devuelve NULL (línea 50 de InstanceLogonService)
-		Assert.assertEquals(entity, service.find(entity.getId()));
+		Assert.assertEquals(1, service.find(entity).size());
+		Assert.assertEquals("Role test name", service.find(entity.getId())
+				.getName());
+		System.out.println("BEFORE " + entity.getId() + " " + entity.getName()
+				+ "\nAFTER " + service.find(entity.getId()).getId() + " "
+				+ service.find(entity.getId()).getName());
+		// Assert.assertEquals(entity, service.find(entity.getId()));
 		Assert.assertTrue(service.getAll() != null
 				&& service.getAll().size() >= 1);
-		if (super.getBpa() == null) {
-			Bpa bpa = new Bpa();
-			em.persist(bpa);
-			em.flush();
-			super.setBpa(em.find(Bpa.class, bpa.getId()));
-		}
-		entity.setBpa(super.getBpa());
+		entity.setName("Role test name updated");
 		entity = service.update(entity);
-		Assert.assertEquals(super.getBpa(), service.find(entity.getId())
-				.getBpa());
+		Assert.assertEquals("Role test name updated",
+				service.find(entity.getId()).getName());
 		long id = entity.getId();
 		service.delete(entity.getId());
 		Assert.assertNull(service.find(id));
