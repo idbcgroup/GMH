318c4058cfb04df0c1ec7ca3b05070ef44849d81
Viviana Torres Guerra <vtorres@stcsolutions.com.ve>
2013-09-28 00:24:45 +0100

    LegalEntityServiceTest agregado. El método find(legalEntity) siempre devuelve NULL en el servicio. A pesar que imprime los mismos id e identifier al hacer assertEquals de los objetos no es válido (líneas 45-58 de LegalEntityServiceTest)

---
+++ ejb/src/test/java/org/fourgeeks/gha/ejb/mix/LegalEntityServiceTest.java
@@ -0,0 +1,78 @@
+package org.fourgeeks.gha.ejb.mix;
+
+import javax.ejb.EJB;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+
+import junit.framework.Assert;
+
+import org.fourgeeks.gha.domain.exceptions.EJBException;
+import org.fourgeeks.gha.domain.mix.LegalEntity;
+import org.fourgeeks.gha.ejb.GhaServiceTest;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author vivi.torresg
+ * 
+ */
+@RunWith(Arquillian.class)
+public class LegalEntityServiceTest extends GhaServiceTest {
+	@PersistenceContext
+	EntityManager em;
+
+	@EJB(name = "mix.LegalEntityService")
+	LegalEntityServiceRemote service;
+
+	@Inject
+	UserTransaction ux;
+
+	@Test
+	public void test() throws NotSupportedException, SystemException,
+			SecurityException, IllegalStateException, RollbackException,
+			HeuristicMixedException, HeuristicRollbackException, EJBException {
+		Assert.assertNotNull(em);
+		Assert.assertNotNull(service);
+
+		ux.begin();
+		em.joinTransaction();
+
+		LegalEntity entity = new LegalEntity();
+		entity.setIdentifier("LegalEntity test identifier");
+
+		entity = service.save(entity);
+
+		Assert.assertNotNull(service.find(entity.getId()));
+		System.out.println("BEFORE " + entity.getId() + " "
+				+ entity.getIdentifier() + "\nAFTER "
+				+ service.find(entity.getId()).getId() + " "
+				+ service.find(entity.getId()).getIdentifier());
+		// Assert.assertEquals(entity, service.find(entity.getId()));
+
+		// Assert.assertTrue(service.find(entity) != null
+		// && service.find(entity).size() >= 1);
+		Assert.assertTrue(service.getAll() != null
+				&& service.getAll().size() >= 1);
+		Assert.assertEquals(service.find(entity.getId()).getIdentifier(),
+				"LegalEntity test identifier");
+		entity.setIdentifier("LegalEntity test identifier updated");
+		entity = service.update(entity);
+		Assert.assertEquals(service.find(entity.getId()).getIdentifier(),
+				"LegalEntity test identifier updated");
+
+		long id = entity.getId();
+		service.delete(entity.getId());
+		Assert.assertNull(service.find(id));
+
+		ux.commit();
+
+	}
+}
