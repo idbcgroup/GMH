897925dbb04c6df6c42a09003df8d665eb6fbad9
Viviana Torres Guerra <vtorres@stcsolutions.com.ve>
2013-09-27 14:00:23 +0100

    EiaTypeMaterialServiceTest agregado. Sólo tiene tres métodos: save(eiaTypeMaterial) delete(id) y findByEiaType(eiaType), funcionan correctamente

---
+++ ejb/src/test/java/org/fourgeeks/gha/ejb/gmh/EiaTypeMaterialServiceTest.java
@@ -0,0 +1,62 @@
+package org.fourgeeks.gha.ejb.gmh;
+
+import javax.ejb.EJB;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+
+import junit.framework.Assert;
+
+import org.fourgeeks.gha.domain.exceptions.EJBException;
+import org.fourgeeks.gha.domain.gmh.EiaTypeMaterial;
+import org.fourgeeks.gha.ejb.GhaServiceTest;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author vivi.torresg
+ * 
+ */
+@RunWith(Arquillian.class)
+public class EiaTypeMaterialServiceTest extends GhaServiceTest {
+	@PersistenceContext
+	EntityManager em;
+
+	@EJB(name = "gmh.EiaTypeMaterialService")
+	EiaTypeMaterialServiceRemote service;
+
+	@Inject
+	UserTransaction ux;
+
+	@Test
+	public void test() throws NotSupportedException, SystemException,
+			SecurityException, IllegalStateException, RollbackException,
+			HeuristicMixedException, HeuristicRollbackException, EJBException {
+		Assert.assertNotNull(em);
+		Assert.assertNotNull(service);
+
+		ux.begin();
+		em.joinTransaction();
+
+		EiaTypeMaterial entity = new EiaTypeMaterial();
+		entity.setEiaType(super.getEiaType(em));
+
+		entity = service.save(entity);
+
+		Assert.assertTrue(service.findByEiaType(entity.getEiaType()) != null
+				&& service.findByEiaType(entity.getEiaType()).size() >= 1);
+		Assert.assertTrue(service.findByEiaType(entity.getEiaType()).size() == 1);
+		service.delete(entity.getId());
+		Assert.assertTrue(service.findByEiaType(entity.getEiaType()).size() == 0);
+
+		ux.commit();
+
+	}
+}
