1e351ab840945bf71d3cb7ba4b34308de1c37b13
Emilio Tirado <etirado@stcsolutions.com.ve>
2013-09-30 17:10:58 -0430

    maintenance protocol utils, falta implementar

---
+++ webclient/src/main/java/org/fourgeeks/gha/webclient/client/maintenanceprotocol/MaintenanceProtocolUtil.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.fourgeeks.gha.webclient.client.maintenanceprotocol;
+
+/**
+ * @author emiliot
+ *
+ */
+public class MaintenanceProtocolUtil {
+	/**
+	 * 
+	 */
+	private MaintenanceProtocolUtil() {
+		throw new UnsupportedOperationException("Esta clase no debe ser instanciada");
+	}
+	
+	
+}
