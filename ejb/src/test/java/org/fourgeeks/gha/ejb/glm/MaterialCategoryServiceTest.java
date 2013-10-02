0fe7f915b020a3df04a8bcd9887b656680d4231b
Viviana Torres Guerra <vtorres@stcsolutions.com.ve>
2013-10-02 23:33:30 +0100

    Correciones en MaterialCategory dominio y servicio. Probados y funcionando.

--- ejb/src/test/java/org/fourgeeks/gha/ejb/glm/MaterialCategoryServiceTest.java
+++ ejb/src/test/java/org/fourgeeks/gha/ejb/glm/MaterialCategoryServiceTest.java
@@ -51,15 +51,15 @@ public class MaterialCategoryServiceTest extends GhaServiceTest {
 		entity = service.save(entity);
 
 		Assert.assertNotNull(entity);
-		// Assert.assertEquals(1, service.find(entity).size());
+		Assert.assertEquals(1, service.find(entity).size());
 		System.out.println("BEFORE " + entity.getId() + " " + entity.getCode()
 				+ "\nAFTER " + service.find(entity.getId()).getId() + " "
 				+ service.find(entity.getId()).getCode());
 		// Assert.assertEquals(entity, service.find(entity.getId()));
-		//Assert.assertTrue(service.getAll() != null
-		//		&& service.getAll().size() >= 1);
-		//Assert.assertTrue(service.getAll(0, 10) != null
-		//		&& service.getAll(0, 10).size() >= 1);
+		Assert.assertTrue(service.getAll() != null
+				&& service.getAll().size() >= 1);
+		Assert.assertTrue(service.getAll(0, 10) != null
+				&& service.getAll(0, 10).size() >= 1);
 		entity.setCode("MaterialCategory test code updated");
 		entity = service.update(entity);
 		Assert.assertEquals("MaterialCategory test code updated",
