ec64d996ed46b6305e4a0f5a67a682f24d486548
Viviana Torres Guerra <vtorres@stcsolutions.com.ve>
2013-10-07 23:24:38 +0100

    EiaTypeMaintenancePlanServiceTest agregado. Funcionando correctamente

---
+++ ejb/src/test/java/org/fourgeeks/gha/ejb/gmh/EiaTypeMaintenancePlanServiceTest.java
@@ -0,0 +1,68 @@
+package org.fourgeeks.gha.ejb.gmh;
+
+import javax.ejb.EJB;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+
+import junit.framework.Assert;
+
+import org.fourgeeks.gha.domain.exceptions.GHAEJBException;
+import org.fourgeeks.gha.domain.gmh.EiaTypeMaintenancePlan;
+import org.fourgeeks.gha.ejb.GhaServiceTest;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author vivi.torresg
+ * 
+ */
+@RunWith(Arquillian.class)
+public class EiaTypeMaintenancePlanServiceTest extends GhaServiceTest {
+	@PersistenceContext
+	EntityManager em;
+
+	@EJB(name = "eiaTypeMaintenancePlanService")
+	EiaTypeMaintenancePlanServiceRemote service;
+
+	@Inject
+	UserTransaction ux;
+
+	@Test
+	public void test() throws NotSupportedException, SystemException,
+			SecurityException, IllegalStateException, RollbackException,
+			HeuristicMixedException, HeuristicRollbackException,
+			GHAEJBException {
+		Assert.assertNotNull(em);
+		Assert.assertNotNull(service);
+
+		ux.begin();
+		em.joinTransaction();
+
+		EiaTypeMaintenancePlan entity = new EiaTypeMaintenancePlan();
+		entity.setEiaType(super.getEiaType(em));
+		entity.setMaintenancePlan(super.getMaintenancePlan(em));
+
+		entity = service.save(entity);
+
+		Assert.assertTrue(service.findByEiaType(entity.getEiaType()) != null
+				&& service.findByEiaType(entity.getEiaType()).size() >= 1);
+		Assert.assertTrue(service.findByMaintenancePlan(entity
+				.getMaintenancePlan()) != null
+				&& service.findByMaintenancePlan(entity.getMaintenancePlan())
+						.size() >= 1);
+		Assert.assertTrue(service.findByEiaType(entity.getEiaType()).size() == 1);
+		service.delete(entity.getId());
+		Assert.assertTrue(service.findByEiaType(entity.getEiaType()).size() == 0);
+
+		ux.commit();
+
+	}
+}
