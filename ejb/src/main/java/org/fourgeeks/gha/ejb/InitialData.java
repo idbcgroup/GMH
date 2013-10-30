fdf1b84a26246234261a78440c8a376c9595cd4a
naramirez <naramirez@4geeks.com.ve>
2013-10-29 19:58:44 -0430

    cambios minimos

--- ejb/src/main/java/org/fourgeeks/gha/ejb/InitialData.java
+++ ejb/src/main/java/org/fourgeeks/gha/ejb/InitialData.java
@@ -166,16 +166,12 @@ public class InitialData {
 						"Guardado exitoso del Equipo"));
 				em.persist(new GHAMessage(LanguageEnum.ES, "eiatype-save-success",
 						"Guardado exitoso del Tipo de Equipo"));
-<<<<<<< HEAD
 				em.persist(new GHAMessage(LanguageEnum.ES, "eiatype-delete-success",
 						"Eliminación exitosa del Tipo de Equipo"));
 				em.persist(new GHAMessage(LanguageEnum.ES, "eiatypes-delete-success",
 						"Eliminación exitosa de los Tipos de Equipo"));
-=======
-				em.persist(new GHAMessage(LanguageEnum.ES,
-						"material-save-success",
+				em.persist(new GHAMessage(LanguageEnum.ES, "material-save-success",
 						"Guardado exitoso del material"));
->>>>>>> master
 				em.persist(new GHAMessage(LanguageEnum.ES, "user-save-success",
 						"Guardado exitoso del Usuario"));
 				em.persist(new GHAMessage(LanguageEnum.ES, "mplan-save-success",
@@ -1158,8 +1154,7 @@ public class InitialData {
 				em.persist(new UiString(LanguageEnum.ES, "save", "Guardar"));
 				em.persist(new UiString(LanguageEnum.ES, "materials-category",
 						"Categorias de materiales"));
-				em.persist(new UiString(LanguageEnum.ES, "new-material",
-						"Nuevo material"));
+				em.persist(new UiString(LanguageEnum.ES, "new-material", "Nuevo material"));
 				em.persist(new UiString(LanguageEnum.ES, "utility-services",
 						"Servicios utilitarios"));
 				em.persist(new UiString(LanguageEnum.ES, "external-code", "Codigo externo"));// TODO
