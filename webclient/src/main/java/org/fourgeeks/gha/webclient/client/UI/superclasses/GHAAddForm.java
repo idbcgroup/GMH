6aff509c9be09e90384dd511b67c3ea8476fec2d
Angel Lacret <alacret@gmail.com>
2013-10-31 15:28:40 -0430

    bug de la posicion del addform de materiales

--- webclient/src/main/java/org/fourgeeks/gha/webclient/client/UI/superclasses/GHAAddForm.java
+++ webclient/src/main/java/org/fourgeeks/gha/webclient/client/UI/superclasses/GHAAddForm.java
@@ -1,8 +1,5 @@
 package org.fourgeeks.gha.webclient.client.UI.superclasses;
 
-import org.fourgeeks.gha.webclient.client.UI.GHAUiHelper;
-
-import com.google.gwt.event.logical.shared.ResizeEvent;
 
 /**
  * @author alacret
@@ -16,14 +13,7 @@ public abstract class GHAAddForm extends GHASlideInWindow {
 	 */
 	public GHAAddForm(String title) {
 		super();
-		setHeight(GHAUiHelper.getTabHeight());
-		setTop(GHAUiHelper.getTopSpace());
 		label = new GHALabel(title);
 		addMember(label);
-	}
-
-	@Override
-	public void onResize(ResizeEvent event) {
-		setHeight(GHAUiHelper.getTabHeight());
 	}
 }
