e374a5ba71b504f003d4fdcae869e04a8fc8d152
Jorge Fuentes <glisterkaiser@gmail.com>
2013-11-06 13:13:41 -0430

    notifications dev2

--- webclient/src/main/java/org/fourgeeks/gha/webclient/client/home/HomePlace.java
+++ webclient/src/main/java/org/fourgeeks/gha/webclient/client/home/HomePlace.java
@@ -4,9 +4,7 @@ import org.fourgeeks.gha.domain.gar.Bpu;
 import org.fourgeeks.gha.webclient.client.UI.GHASessionData;
 import org.fourgeeks.gha.webclient.client.UI.GHAUiHelper;
 import org.fourgeeks.gha.webclient.client.UI.dropdownmenus.UserDropdownMenu;
-import org.fourgeeks.gha.webclient.client.UI.superclasses.GHAButton;
 import org.fourgeeks.gha.webclient.client.UI.superclasses.GHALabel;
-import org.fourgeeks.gha.webclient.client.UI.superclasses.GHANotification;
 import org.fourgeeks.gha.webclient.client.UI.tabs.GHAPlace;
 import org.fourgeeks.gha.webclient.client.UI.tabs.GHATabSet;
 
@@ -16,7 +14,6 @@ import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.types.Cursor;
 import com.smartgwt.client.widgets.Img;
 import com.smartgwt.client.widgets.events.ClickEvent;
-import com.smartgwt.client.widgets.events.ClickHandler;
 import com.smartgwt.client.widgets.events.FocusChangedEvent;
 import com.smartgwt.client.widgets.events.FocusChangedHandler;
 import com.smartgwt.client.widgets.layout.HLayout;
@@ -95,17 +92,20 @@ public class HomePlace extends GHAPlace {
 					}
 				});
 		
-		GHAButton notifbut= new GHAButton("Show", new ClickHandler() {
-			
-			@Override
-			public void onClick(ClickEvent event) {
-				// TODO Auto-generated method stub
-				GHANotification.modalNotification.show("Informeichon", "Informacion del error");
-			}
-		});	
+//		GHAButton notifbut= new GHAButton("Show", new ClickHandler() {
+//			
+//			@Override
+//			public void onClick(ClickEvent event) {
+//				// TODO Auto-generated method stub
+//				GHANotification.modalNotification.show("Informeichon", "Informacion del error");
+//			}
+//		});	
 
-		userInfo.addMembers(usernameLabel, /* notificationsButton, */
-				userButton,notifbut);
+		userInfo.addMembers(usernameLabel, 
+				/* notificationsButton, */
+				userButton
+//				, notifbut
+				);
 
 		userInfo.addFocusChangedHandler(new FocusChangedHandler() {
 
