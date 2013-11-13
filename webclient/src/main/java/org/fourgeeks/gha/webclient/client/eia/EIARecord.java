25a2766a8f71d87aa333540f96f5c64e4df72ea8
naramirez <naramirez@4geeks.com.ve>
2013-11-13 13:41:59 -0430

    agregando detailViewer para los datos restantes del grid de eia

--- webclient/src/main/java/org/fourgeeks/gha/webclient/client/eia/EIARecord.java
+++ webclient/src/main/java/org/fourgeeks/gha/webclient/client/eia/EIARecord.java
@@ -1,7 +1,11 @@
 package org.fourgeeks.gha.webclient.client.eia;
 
+import org.fourgeeks.gha.domain.ess.Role;
+import org.fourgeeks.gha.domain.gar.Obu;
+import org.fourgeeks.gha.domain.gmh.Brand;
 import org.fourgeeks.gha.domain.gmh.Eia;
 import org.fourgeeks.gha.domain.gmh.EiaType;
+import org.fourgeeks.gha.domain.mix.Bpi;
 import org.fourgeeks.gha.webclient.client.UI.grids.GHAGridRecord;
 
 /**
@@ -19,8 +23,8 @@ public class EIARecord extends GHAGridRecord<Eia> {
 		this.eia = eia;
 
 		setAttribute("id", eia.getId());
-		setAttribute("code", eia.getCode());
 		setAttribute("serialNumber", eia.getSerialNumber());
+		setAttribute("fai", eia.getFixedAssetIdentifier());
 		setAttribute("state", eia.getState());
 		if (eia.getWorkingArea() != null) {
 			setAttribute("location", eia.getWorkingArea().getName());
@@ -28,9 +32,25 @@ public class EIARecord extends GHAGridRecord<Eia> {
 			setAttribute("location", eia.getFacility().getName());
 		}
 
+		Role role = eia.getResponsibleRole();
+		if (role != null)
+			setAttribute("role", role.getName());
+
+		Obu obu = eia.getObu();
+		if (obu != null) {
+			setAttribute("obu", obu.getName());
+			Bpi bpi = obu.getBpi();
+			if (bpi != null)
+				setAttribute("bpi", bpi.getInstitution().getName());
+		}
+
 		EiaType eiaType = eia.getEiaType();
 		if (eiaType != null) {
-			setAttribute("name", eiaType.getName());
+			setAttribute("type", eiaType.getName());
+			setAttribute("model", eiaType.getModel());
+			Brand brand = eiaType.getBrand();
+			if (brand != null)
+				setAttribute("brand", brand.getName());
 		}
 	}
 
