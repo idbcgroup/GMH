9899474afbde348ed459c973a67535caf97630c5
Emilio Tirado <etirado@stcsolutions.com.ve>
2013-11-21 12:01:03 -0430

    domain de la planificacion de mantenimiento

---
+++ domain/src/main/java/org/fourgeeks/gha/domain/gmh/EiaCorrectiveMaintenancePlanification.java
@@ -0,0 +1,47 @@
+package org.fourgeeks.gha.domain.gmh;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import org.fourgeeks.gha.domain.AbstractEntity;
+
+/**
+ * @author emiliot
+ * 
+ */
+public class EiaCorrectiveMaintenancePlanification extends AbstractEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@OneToOne
+	@JoinColumn(name = "eiaMaintenancePlanificationFk")
+	private EiaMaintenancePlanification planification;
+
+	// TODO agregar una clave con el reporte de daño
+
+	/**
+	 * 
+	 */
+	public EiaCorrectiveMaintenancePlanification() {
+		super();
+	}
+
+	/**
+	 * @return the planification
+	 */
+	public EiaMaintenancePlanification getPlanification() {
+		return planification;
+	}
+
+	/**
+	 * @param planification
+	 *            the planification to set
+	 */
+	public void setPlanification(EiaMaintenancePlanification planification) {
+		this.planification = planification;
+	}
+
+}
---
+++ domain/src/main/java/org/fourgeeks/gha/domain/gmh/EiaPreventiveMaintenancePlanification.java
@@ -0,0 +1,65 @@
+package org.fourgeeks.gha.domain.gmh;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import org.fourgeeks.gha.domain.AbstractEntity;
+
+/**
+ * @author emiliot
+ * 
+ */
+public class EiaPreventiveMaintenancePlanification extends AbstractEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@ManyToOne
+	@JoinColumn(name = "eiaTypeMaintenancePlanFk")
+	private EiaTypeMaintenancePlan plan;
+
+	@OneToOne
+	@JoinColumn(name = "eiaMaintenancePlanificationFk")
+	private EiaMaintenancePlanification planification;
+
+	/**
+	 * 
+	 */
+	public EiaPreventiveMaintenancePlanification() {
+		super();
+	}
+
+	/**
+	 * @return the plan
+	 */
+	public EiaTypeMaintenancePlan getPlan() {
+		return plan;
+	}
+
+	/**
+	 * @param plan
+	 *            the plan to set
+	 */
+	public void setPlan(EiaTypeMaintenancePlan plan) {
+		this.plan = plan;
+	}
+
+	/**
+	 * @return the planification
+	 */
+	public EiaMaintenancePlanification getPlanification() {
+		return planification;
+	}
+
+	/**
+	 * @param planification
+	 *            the planification to set
+	 */
+	public void setPlanification(EiaMaintenancePlanification planification) {
+		this.planification = planification;
+	}
+
+}
