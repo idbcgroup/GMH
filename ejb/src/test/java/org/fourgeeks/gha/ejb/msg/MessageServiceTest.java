040c4311c035e29c187d8fa831534a9c775fad06
Glis-hocs <jfuentes@stcsolutions.com.ve>
2014-02-07 18:37:21 -0430

    sms testing

--- ejb/src/test/java/org/fourgeeks/gha/ejb/msg/MessageServiceTest.java
+++ ejb/src/test/java/org/fourgeeks/gha/ejb/msg/MessageServiceTest.java
@@ -6,36 +6,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.EJB;
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.transaction.HeuristicMixedException;
 import javax.transaction.HeuristicRollbackException;
 import javax.transaction.NotSupportedException;
 import javax.transaction.RollbackException;
 import javax.transaction.SystemException;
-import javax.transaction.UserTransaction;
 
 import org.fourgeeks.gha.domain.enu.LanguageEnum;
 import org.fourgeeks.gha.domain.exceptions.GHAEJBException;
 import org.fourgeeks.gha.domain.msg.GHAMessage;
+import org.fourgeeks.gha.domain.msg.GHAMessageType;
 import org.fourgeeks.gha.ejb.GhaServiceTest;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
- * @author emiliot
+ * @author emiliot, jfuentes
  * 
  */
 
-// @RunWith(Arquillian.class)
+@RunWith(Arquillian.class)
 public class MessageServiceTest extends GhaServiceTest {
-	@PersistenceContext
-	EntityManager em;
+
+	@Deployment
+	public static Archive<?> createDeployment() {
+		return ShrinkWrap.create(WebArchive.class, "test.war")
+				//Clases
+				.addAsResource("test-persistence.xml",
+						"META-INF/persistence.xml")
+						.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+
 
 	@EJB(name = "msg.messageService")
-	MessageServiceRemote service;
+	MessageServiceLocal messageServiceLocal;
 
-	@Inject
-	UserTransaction ux;
+
+	@EJB(name = "msg.messageService")
+	MessageServiceRemote messageServiceRemote;
+
 
 	/**
 	 * @throws NotSupportedException
@@ -47,25 +62,24 @@ public class MessageServiceTest extends GhaServiceTest {
 	 * @throws HeuristicRollbackException
 	 * @throws GHAEJBException
 	 */
-	// @Test
-	public void test() throws NotSupportedException, SystemException,
-			SecurityException, IllegalStateException, RollbackException,
-			HeuristicMixedException, HeuristicRollbackException,
-			GHAEJBException {
-		assertNotNull(em);
-		assertNotNull(service);
+	@Test
+	public void test() {
+		assertNotNull(messageServiceLocal);
+		assertNotNull(messageServiceRemote);
 
-		ux.begin();
-		em.joinTransaction();
+		GHAMessageType ghaMessageType = new GHAMessageType();
+		ghaMessageType.setTypeName("SAY");
+
+		GHAMessage ghaMessage = new GHAMessage("unit-test-message",LanguageEnum.ES);
+		ghaMessage.setText("ghaMessage unit test");
+		ghaMessage.setType(ghaMessageType);
+
 
 		try {
 			final String codes[] = { "test-code", "test-code2" };
 			final String msgs[] = { "test msg", "test msg 2" };
-			em.persist(new GHAMessage(LanguageEnum.ES, codes[0], msgs[0]));
-			em.persist(new GHAMessage(LanguageEnum.ES, codes[1], msgs[1]));
-			em.flush();
 
-			final GHAMessage msg = service.find(codes[0]);
+			final GHAMessage msg = messageServiceRemote.find(codes[0]);
 			assertNotNull(msg);
 			assert (msg.getText().equals(msgs[0]));
 
@@ -73,7 +87,7 @@ public class MessageServiceTest extends GhaServiceTest {
 			for (final String s : codes)
 				param.add(s);
 
-			final List<GHAMessage> test = service.find(param);
+			final List<GHAMessage> test = messageServiceRemote.find(param);
 			assertNotNull(test);
 			int i = 0;
 			for (final GHAMessage message : test) {
@@ -81,10 +95,8 @@ public class MessageServiceTest extends GhaServiceTest {
 			}
 
 		} catch (final Exception e) {
-			ux.rollback();
 			e.printStackTrace();
 		}
 
-		ux.commit();
 	}
 }
