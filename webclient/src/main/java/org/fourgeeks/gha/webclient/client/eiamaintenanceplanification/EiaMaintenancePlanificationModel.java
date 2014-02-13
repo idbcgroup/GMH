2864c7d80de25ea45ff1e30e249564535fbb56d6
Angel Lacret <alacret@stcsolutions.com.ve>
2014-02-13 12:55:34 -0430

    bug a eia*mainte*planif*model

--- webclient/src/main/java/org/fourgeeks/gha/webclient/client/eiamaintenanceplanification/EiaMaintenancePlanificationModel.java
+++ webclient/src/main/java/org/fourgeeks/gha/webclient/client/eiamaintenanceplanification/EiaMaintenancePlanificationModel.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.fourgeeks.gha.domain.gmh.EiaMaintenancePlanification;
 import org.fourgeeks.gha.domain.gmh.EiaType;
 import org.fourgeeks.gha.webclient.client.UI.GHAAsyncCallback;
-import org.fourgeeks.gha.webclient.client.eiapreventivemaintenanceplanification.GWTMaintenancePlanificationServiceAsync;
 
 import com.google.gwt.core.shared.GWT;
 
