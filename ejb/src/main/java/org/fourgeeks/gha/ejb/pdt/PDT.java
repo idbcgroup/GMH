aa4335228ffd3516ab586538563881b9617fa846
naramirez <naramirez@4geeks.com.ve>
2014-02-23 19:35:11 -0430

    Quitando comentarios para debug

--- ejb/src/main/java/org/fourgeeks/gha/ejb/pdt/PDT.java
+++ ejb/src/main/java/org/fourgeeks/gha/ejb/pdt/PDT.java
@@ -35,26 +35,18 @@ public class PDT implements MessageListener {
 	@SuppressWarnings("unchecked")
 	public void onMessage(Message message) {
 		try {
-			logger.log(Level.INFO, "ZZZ 1");
 			ObjectMessage objectMessage = (ObjectMessage) message;
-			logger.log(Level.INFO, "ZZZ 2");
 			HashMap<String, Object> data = (HashMap<String, Object>) objectMessage
 					.getObject();
-			logger.log(Level.INFO, "ZZZ 3");
 
 			String code = (String) data.get("code");
-			logger.log(Level.INFO, "ZZZ 4");
 			TransactionParams transactParams = transParamsService.find(code);
-			logger.log(Level.INFO, "ZZZ 5");
 
 			Context jndiContext = new InitialContext();
-			logger.log(Level.INFO, "ZZZ 6");
 			PDTProcessor processor = (PDTProcessor) jndiContext
 					.lookup(transactParams.getJndiProcessorName());
-			logger.log(Level.INFO, "ZZZ 7");
 
 			processor.processMessage(data);
-			logger.log(Level.INFO, "ZZZ 8");
 
 		} catch (Exception e) {
 			logger.log(Level.INFO, "Error al consumir el mensaje en PDT: ", e);
