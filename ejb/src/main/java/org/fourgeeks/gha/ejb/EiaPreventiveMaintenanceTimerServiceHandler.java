8a4ab9d5ca33a344265054feca43374d4c99ce39
naramirez <naramirez@4geeks.com.ve>
2014-03-04 22:48:58 -0430

    quitando mensajes para hacer debug

--- ejb/src/main/java/org/fourgeeks/gha/ejb/EiaPreventiveMaintenanceTimerServiceHandler.java
+++ ejb/src/main/java/org/fourgeeks/gha/ejb/EiaPreventiveMaintenanceTimerServiceHandler.java
@@ -39,26 +39,22 @@ public class EiaPreventiveMaintenanceTimerServiceHandler implements
 
 	@Override
 	public void doWork(TimerParams timerParams) {
-		logger.log(Level.INFO,
-				"entro en el EiaPreventiveMaintenanceTimerServiceHandler");
 		try {
 			Calendar calendar = Calendar.getInstance();
 			List<EiaMaintenancePlanification> planifs = planifService.getAll();
-
-			logger.log(Level.INFO, "planifs.size = " + planifs.size());
 
 			// para cada planificacion de mantenimiento
 			for (EiaMaintenancePlanification planif : planifs) {
 				// si el equipo no esta EN OPERACION no se programan nuevos
 				// mantenimientos
 				Eia eia = planif.getEia();
-				logger.log(Level.INFO, "eia.getState() = " + eia.getState());
 				if (eia.getState() != EiaStateEnum.IN_OPERATION)
 					continue;
 
 				// obtengo la fecha del ultimo mantenimiento programado
 				Date lastMaintenanceDate = planifService
 						.getScheduleDateOfLastMaintenance(planif);
+
 				// obtengo la fecha de inicio de la planificacion
 				Date beginningDate = planif.getBeginningDate();
 
@@ -67,20 +63,8 @@ public class EiaPreventiveMaintenanceTimerServiceHandler implements
 				long lastScheduleTime = (lastMaintenanceDate == null) ? beginningDate
 						.getTime() : lastMaintenanceDate.getTime();
 
-				// verifico si la fecha actual es mayor que la ultima de fecha
-				// de programacion
+				// si ya paso la fecha del ultimo mantenimiento programado
 				long actualTime = calendar.getTimeInMillis();
-
-				logger.log(Level.INFO,
-						"beginningDateTime = " + beginningDate.getTime());
-				logger.log(Level.INFO, "lastMaintenanceDateTime = "
-						+ (lastMaintenanceDate == null ? "es nulo"
-								: lastMaintenanceDate.getTime()));
-				logger.log(Level.INFO, "actualTime = " + actualTime
-						+ " - lastScheduleTime = " + lastScheduleTime);
-				logger.log(Level.INFO, "actualTime >= lastScheduleTime? "
-						+ (actualTime >= lastScheduleTime));
-
 				if (actualTime >= lastScheduleTime) {
 					calendar.setTimeInMillis(lastScheduleTime);
 					MaintenancePlan maintenancePlan = planif.getPlan()
@@ -96,41 +80,29 @@ public class EiaPreventiveMaintenanceTimerServiceHandler implements
 							timerParams.getDuration(),
 							timerParams.getDurationPot());
 
-					// obtnego la cantidad de mantenimientos que se pueden
-					// programar en el rango de tiempo dado
+					// cantidad de mantenimientos que se pueden programar en el
+					// rango de tiempo dado
 					int cantOfMaintenances = timeRange / planFrequency;
-
-					logger.log(Level.INFO, "timeRange= " + timeRange
-							+ " - planFrequency = " + planFrequency);
-					logger.log(Level.INFO,
-							"cantOfMaintenances (timeRange / planFrequency) = "
-									+ cantOfMaintenances);
 
 					// realizo la programacion de las nuevas planificaciones
 					for (int i = 0; i < cantOfMaintenances; i++) {
 						// nueva fecha de programación del mantenimiento
 						calendar.add(Calendar.SECOND, planFrequency);
 
-						// preparo los datos a ser enviados al PDT
+						// se preparan los datos a ser enviados al PDT
 						HashMap<String, Object> params = new HashMap<String, Object>();
-						params.put("scheduleDate", calendar.getTimeInMillis());
 						params.put("planif", planif);
+						params.put("scheduleDate", calendar.getTimeInMillis());
 
 						// envio los datos al PDT
-						logger.log(Level.INFO, "calendar.getTime() = "
-								+ calendar.getTime());
-
-						messageService.sendMessage("preventive-maintenance",
-								params);
-
-						logger.log(Level.INFO, "se mandó el mensaje");
+						String transactionCode = "preventive-maintenance";
+						messageService.sendMessage(transactionCode, params);
 					}
 				}
-
 			}
-
 		} catch (GHAEJBException e) {
-			e.printStackTrace();
+			String msg = "ERROR: problemas en EiaPreventiveMaintenanceTimerServiceHandler: ";
+			logger.log(Level.INFO, msg, e);
 		}
 	}
 }
