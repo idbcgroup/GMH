0954a24c4b154665d5251938efff4bfb7457f97d
naramirez <naramirez@4geeks.com.ve>
2014-04-09 11:45:58 -0430

    cambios en paquete UI

--- webclient/src/main/java/org/fourgeeks/gha/webclient/client/UI/grids/tree/GHATreeGridNode.java
+++
@@ -1,8 +0,0 @@
-package org.fourgeeks.gha.webclient.client.UI.grids.tree;
-
-import com.smartgwt.client.widgets.tree.TreeNode;
-
-public abstract class GHATreeGridNode<T> extends TreeNode {
-
-	public abstract T toEntity();
-}
