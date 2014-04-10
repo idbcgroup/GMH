81d2eef740a0d95874e0e74b1d778adb671d7dec
eguerere <eguerere@4geeks.com.ve>
2014-04-10 14:22:02 -0430

    Se agrego metodo para eliminar mas de un tipo de quipo asociado en el plan de mantenimiento

--- webclient/src/main/java/org/fourgeeks/gha/webclient/client/maintenanceplan/asociatedeiatype/GWTEiaTypeMaintenancePlanService.java
+++ webclient/src/main/java/org/fourgeeks/gha/webclient/client/maintenanceplan/asociatedeiatype/GWTEiaTypeMaintenancePlanService.java
@@ -15,32 +15,47 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 /**
  * @author emiliot
- *
+ * 
  */
 
 @RemoteServiceRelativePath("eiaTypeMaintenancePlanService")
-public interface GWTEiaTypeMaintenancePlanService extends RemoteService{
+public interface GWTEiaTypeMaintenancePlanService extends RemoteService {
 	/**
 	 * Delete a EiaTypeMaintenancePlan from database by Id
+	 * 
 	 * @throws GHAEJBException
 	 */
 	public void delete(long Id) throws GHAEJBException;
+
+	/**
+	 * @param eiatypemaintenanceplan
+	 * @throws GHAEJBException
+	 */
+	public void delete(List<EiaTypeMaintenancePlan> eiatypemaintenanceplan)
+			throws GHAEJBException;
+
 	/**
 	 * @param eiaTypeMaintenancePlan
 	 * @return the persistent entity
 	 * @throws GHAEJBException
 	 */
-	public EiaTypeMaintenancePlan save(EiaTypeMaintenancePlan eiaTypeMaintenancePlan) throws GHAEJBException;
+	public EiaTypeMaintenancePlan save(
+			EiaTypeMaintenancePlan eiaTypeMaintenancePlan)
+			throws GHAEJBException;
+
 	/**
 	 * @param eiaType
 	 * @return the eiatypemaintenance plan associated with the eiatype
 	 * @throws GHAEJBException
 	 */
-	public List<EiaTypeMaintenancePlan> findByEiaType(EiaType eiaType) throws GHAEJBException;
+	public List<EiaTypeMaintenancePlan> findByEiaType(EiaType eiaType)
+			throws GHAEJBException;
+
 	/**
 	 * @param maintenancePlan
 	 * @return the eiatypemaintenance plan associated with the plan
 	 * @throws GHAEJBException
 	 */
-	public List<EiaTypeMaintenancePlan> findByMaintenancePlan(MaintenancePlan maintenancePlan) throws GHAEJBException;
+	public List<EiaTypeMaintenancePlan> findByMaintenancePlan(
+			MaintenancePlan maintenancePlan) throws GHAEJBException;
 }
